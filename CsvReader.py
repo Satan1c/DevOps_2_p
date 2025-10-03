7160769c0ad429d50402fe6965df319838786026
Satan1c <satan1c@ukr.net>
2025-10-03 20:52:39 +0300

    Initial commit

---
+++ CsvReader.py
@@ -0,0 +1,15 @@
+﻿import csv
+from io import StringIO
+
+import requests
+
+from DataReader import DataReader
+
+
+class CsvReader(DataReader):
+	def read_data(self, path):
+		"""Читання даних з CSV файлу за URL."""
+		response = requests.get(path)
+		response_text = StringIO(response.content.decode('utf-8-sig'))
+		reader = csv.DictReader(response_text, delimiter=',')
+		return list(reader)
